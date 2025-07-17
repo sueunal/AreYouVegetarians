da49283f0f79ab4121e2e1436f8cfd8b195a34bb
soom <soom@soomui-MacBookPro.local>
2025-07-17 21:43:19 +0900

    [init]: init commit

---
+++ AreYouVegetarians/AreYouVegetarians/AreYouVegetariansApp.swift
@@ -0,0 +1,17 @@
+//
+//  AreYouVegetariansApp.swift
+//  AreYouVegetarians
+//
+//  Created by soom on 7/17/25.
+//
+
+import SwiftUI
+
+@main
+struct AreYouVegetariansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
