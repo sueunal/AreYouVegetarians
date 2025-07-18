b80d33cbf77619f2776d7933a38b5d122e8c3a7f
soom <soom@soomui-MacBookPro.local>
2025-07-18 21:11:14 +0900

    [test]

---
+++ AreYouVegetarians/AreYouVegetariansTests/AreYouVegetariansTests.swift
@@ -0,0 +1,14 @@
+//
+//  AreYouVegetariansTests.swift
+//  AreYouVegetariansTests
+//
+//  Created by soom on 7/18/25.
+//
+
+import XCTest
+
+final class AreYouVegetariansTests: XCTestCase {
+    func testExample() throws {
+        XCTAssertEqual(1 + 1, 2)
+    }
+}
