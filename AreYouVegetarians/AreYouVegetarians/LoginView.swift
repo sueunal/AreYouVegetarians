bd2fffce0bdb467f86b882ed0b7c693b9ff8f71e
soom <soom@soomui-MacBookPro.local>
2025-07-20 20:49:14 +0900

    [design]: Login View Destionation ContentView

--- AreYouVegetarians/AreYouVegetarians/LoginView.swift
+++ AreYouVegetarians/AreYouVegetarians/LoginView.swift
@@ -10,54 +10,82 @@ import SwiftUI
 struct LoginView: View {
     @State private var id: String = ""
     @State private var pw: String = ""
+    @State private var isLoading: Bool = false
+    @State private var isLoggined: Bool = false
     var body: some View {
-        VStack {
-            
-            Spacer()
-                .frame(height: 50)
-            
-            Text("Login")
-                .fontDesign(.monospaced)
-                .fontWeight(.semibold)
-                .font(.system(size: 32))
-                .foregroundStyle(.blue.opacity(0.9))
-            
-            Spacer()
-                .frame(height: 80)
-            
-            TextField("", text: $id)
-                .padding()
-                .background(
-                    RoundedRectangle(cornerRadius: 8)
-                        .strokeBorder(Color.blue.opacity(0.8), lineWidth: 0.5)
-                )
-            
-            TextField("", text: $pw)
-                .padding()
-                .background(
-                    RoundedRectangle(cornerRadius: 8)
-                        .strokeBorder(Color.blue.opacity(0.8), lineWidth: 0.5)
-                )
-            Button {
+        ZStack {
+            if isLoading {
+                LoadingView(isLoading: $isLoading)
+                    .ignoresSafeArea()
+            }
+            VStack {
+                Spacer()
+                    .frame(height: 50)
                 
-            } label: {
-                Text("Cornfirm")
+                Text("Login")
+                    .fontDesign(.monospaced)
+                    .fontWeight(.semibold)
+                    .font(.system(size: 32))
+                    .foregroundStyle(.blue.opacity(0.9))
+                
+                Spacer()
+                    .frame(height: 80)
+                
+                TextField("", text: $id)
                     .padding()
-                    .foregroundStyle(.white)
-                    .frame(maxWidth: .infinity)
                     .background(
                         RoundedRectangle(cornerRadius: 8)
-                            .foregroundStyle(.blue.opacity(0.6))
+                            .strokeBorder(Color.blue.opacity(0.8), lineWidth: 0.5)
                     )
+                
+                TextField("", text: $pw)
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 8)
+                            .strokeBorder(Color.blue.opacity(0.8), lineWidth: 0.5)
+                    )
+                Button {
+                    isLoading = true
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                       isLoading = false
+                        isLoggined = true
+                    }
+                } label: {
+                    Text("Cornfirm")
+                        .padding()
+                        .foregroundStyle(.white)
+                        .frame(maxWidth: .infinity)
+                        .background(
+                            RoundedRectangle(cornerRadius: 8)
+                                .foregroundStyle(.blue.opacity(0.6))
+                        )
+                }
+                
+                Spacer()
+                
             }
-            
-            Spacer()
-            
+            .padding(.horizontal, 16)
         }
-        .padding(.horizontal, 16)
+        .navigationDestination(isPresented: $isLoggined ) {
+            ContentView()
+                .navigationBarBackButtonHidden(true)
+        }
+    }
+}
+
+
+struct LoadingView: View {
+    @Binding var isLoading: Bool
+    var body: some View {
+        ZStack {
+            Color.black.opacity(0.3)
+            ProgressView()
+        }
     }
 }
 
 #Preview {
-    LoginView()
+    NavigationStack {
+        LoginView()
+    }
 }
