f0c8256d774e2149aeae37156e3c257ac20bf599
soom <soom@soomui-MacBookPro.local>
2025-07-20 20:32:28 +0900

    [design]: login View design

--- AreYouVegetarians/AreYouVegetarians/LoginView.swift
+++ AreYouVegetarians/AreYouVegetarians/LoginView.swift
@@ -8,7 +8,53 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var id: String = ""
+    @State private var pw: String = ""
     var body: some View {
+        VStack {
+            
+            Spacer()
+                .frame(height: 50)
+            
+            Text("Login")
+                .fontDesign(.monospaced)
+                .fontWeight(.semibold)
+                .font(.system(size: 32))
+                .foregroundStyle(.blue.opacity(0.9))
+            
+            Spacer()
+                .frame(height: 80)
+            
+            TextField("", text: $id)
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 8)
+                        .strokeBorder(Color.blue.opacity(0.8), lineWidth: 0.5)
+                )
+            
+            TextField("", text: $pw)
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 8)
+                        .strokeBorder(Color.blue.opacity(0.8), lineWidth: 0.5)
+                )
+            Button {
+                
+            } label: {
+                Text("Cornfirm")
+                    .padding()
+                    .foregroundStyle(.white)
+                    .frame(maxWidth: .infinity)
+                    .background(
+                        RoundedRectangle(cornerRadius: 8)
+                            .foregroundStyle(.blue.opacity(0.6))
+                    )
+            }
+            
+            Spacer()
+            
+        }
+        .padding(.horizontal, 16)
     }
 }
 
