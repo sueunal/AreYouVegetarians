064923048b33eeb0641c3382cf315b3403627273
soom <soom@soomui-MacBookPro.local>
2025-07-20 20:19:14 +0900

    [add]: LoginView

---
+++ AreYouVegetarians/AreYouVegetarians/LoginView.swift
@@ -0,0 +1,17 @@
+//
+//  LoginView.swift
+//  AreYouVegetarians
+//
+//  Created by soom on 7/20/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+    }
+}
+
+#Preview {
+    LoginView()
+}
